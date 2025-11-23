2b61954cca435c1d8f1d9f14c4b4755e6e6c3642
EmmaC1804 <114242779+EmmaC1804@users.noreply.github.com>
2025-11-23 12:09:42 +0000

    Completed Fan out implementation - added emailing logic

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -27,6 +27,10 @@ export class EDAAppStack extends cdk.Stack {
       receiveMessageWaitTime: cdk.Duration.seconds(10),
     })
 
+    const mailerQ = new sqs.Queue(this, "mailer-q", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    })
+
     const newImageTopic = new sns.Topic(this, "NewImageTopic", {
       displayName: "New Image Topic",
     });
@@ -43,6 +47,13 @@ export class EDAAppStack extends cdk.Stack {
       }
     )
 
+    const mailerFn = new lambdanode.NodejsFunction(this, "mailer", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+    })
+
     //S3 -> SQS
     imagesBucket.addEventNotification(
       s3.EventType.OBJECT_CREATED,
@@ -53,16 +64,40 @@ export class EDAAppStack extends cdk.Stack {
       new subs.SqsSubscription(imageProcessQueue)
     );
 
+    newImageTopic.addSubscription(
+      new subs.SqsSubscription(mailerQ)
+    )
+
     //SQS -> Lambda
     const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
       batchSize: 5,
       maxBatchingWindow: cdk.Duration.seconds(10),
     });
 
+    const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(5),
+    }); 
+
     processImageFn.addEventSource(newImageEventSource);
+
+    mailerFn.addEventSource(newImageMailEventSource);
 
     //Permissions
     imagesBucket.grantRead(processImageFn)
+
+    mailerFn.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "ses:SendEmail",
+          "ses:SendRawEmail",
+          "ses:SendTemplatedEmail",
+        ],
+        resources: ["*"],
+      })
+    );
+
 
     // Output
     new cdk.CfnOutput(this, "bucketName", {
